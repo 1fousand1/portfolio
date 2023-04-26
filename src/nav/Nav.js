30f322cda597dce323be5f279c0fc6ffa74c6ae1
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2023-04-26 21:52:45 +0300

    First commit

---
+++ src/nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './Header.module.css';
+
+function Nav() {
+    return (
+        <div className={style.nav}>
+        </div>
+    );
+}
+
+export default Nav;
+///23.58
